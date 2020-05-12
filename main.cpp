a7980f57163127ceca24aee0b4b96a6d65dd2917
Egor Gabov <Egor.Gabov@waveaccess.ru>
2020-05-12 18:48:21 +0300

    Added error handling

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include "LinterCombine.h"
-#include "LinterWrapperBase.h"
+
 #include <iostream>
-#include <memory>
 
 int main( const int argc, char * argv[] ) {
-    LintCombine::LinterCombine linterCombine(argc, argv);
+    try {
+        LintCombine::LinterCombine linterCombine( argc, argv );
+        linterCombine.callLinter();
+        int linterReturnCode = linterCombine.waitLinter();
+        if( linterReturnCode == 1 ) {
+            std::cerr << "some linters are failed" << std::endl;
+        }
+        if( linterReturnCode == 3 ) {
+            std::cerr << "all linters are failed" << std::endl;
+        }
+        LintCombine::CallTotals callTotals = linterCombine.updateYaml();
+        if( callTotals.fail ) {
+            std::cout << "Updating " << callTotals.fail << " yaml-files was failed" << std::endl;
+        }
+    }
+    catch( const std::logic_error & ex ) {
+        std::cerr << ex.what() << std::endl;
+    }
     return 0;
 }
