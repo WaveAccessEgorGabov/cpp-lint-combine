58072fc74dfcbdf4c5053a854238e6a1780e1c1b
Egor Gabov <egor.gabov@waveaccess.ru>
2020-06-09 19:46:16 +0300

    Refactoring of prepareCommandLine

--- src/LintCombineUtils.cpp
+++ src/LintCombineUtils.cpp
@@ -3,52 +3,91 @@
 #include <boost/program_options.hpp>
 #include <boost/algorithm/string/replace.hpp>
 
-void LintCombine::prepareCommandLineForReSharper(LintCombine::stringVector& commandLine) {
+void LintCombine::CommandLineOptions::addOptionToClangTidy( const std::string & option ) {
+    lintersOptions[0]->options.emplace_back(option);
+}
+
+void LintCombine::CommandLineOptions::addOptionToAllLinters( const std::string& option ) {
+    for( const auto & it : lintersOptions ) {
+        it->options.emplace_back( option );
+    }
+}
+
+void LintCombine::CommandLineOptions::appendLintersOptionToCommandLine( std::vector < std::string > & commandLine ) {
+    for ( const auto & i : lintersOptions ) {
+        for(const auto & j : i->options) {
+            commandLine.emplace_back( j );
+        }
+    }
+}
+
+void LintCombine::CommandLineOptions::initLintCombineOptions( std::vector < std::string >& commandLine ) const {
+    commandLine.emplace_back("--result-yaml=" + pathToCommonYaml);
+}
+
+void LintCombine::CommandLineOptions::initUnrecognizedOptions() {
+    for (auto & unrecognized : unrecognizedCollection) {
+        boost::algorithm::replace_all(unrecognized, "\"", "\\\"");
+        std::string strToCompare = "-config=";
+        if ( unrecognized.find( strToCompare ) == 0 ) {
+            unrecognized = optionValueToQuotes(strToCompare, unrecognized);
+            addOptionToClangTidy(unrecognized);
+            continue;
+        }
+        strToCompare = "-line-filter=";
+        if (unrecognized.find(strToCompare) == 0) {
+            unrecognized = optionValueToQuotes(strToCompare, unrecognized);
+            addOptionToClangTidy(unrecognized);
+            continue;
+        }
+        strToCompare = "-header-filter=";
+        if (unrecognized.find(strToCompare) == 0) {
+            addOptionToAllLinters(unrecognized);
+            continue;
+        }
+        if (unrecognized[0] != '@' && unrecognized[0] != '-') {
+            addOptionToAllLinters(unrecognized);
+            continue;
+        }
+        addOptionToClangTidy(unrecognized);
+    }
+}
+
+void LintCombine::CommandLineOptions::initCommandLine( std::vector < std::string > & commandLine ) {
+    commandLine.clear();
+    lintersOptions = { new ClangTidyOptions( pathToWorkDir ) , new ClazyOptions( pathToWorkDir ) };
+    initUnrecognizedOptions();
+    initLintCombineOptions(commandLine);
+    appendLintersOptionToCommandLine( commandLine );
+}
+
+std::string LintCombine::CommandLineOptions::optionValueToQuotes(const std::string & optionName,
+                                       const std::string & optionNameWithValue) {
+    return optionName +"\"" +
+        optionNameWithValue.substr(optionName.size(), std::string::npos) + "\"";
+}
+
+void LintCombine::prepareCommandLineForReSharper( stringVector & commandLine ) {
+    CommandLineOptions commandLineOptions;
     boost::program_options::options_description programOptions;
     std::string pathToResultYaml;
     std::string pathToDiagnosticsDir;
     programOptions.add_options()
-        ("export-fixes", boost::program_options::value < std::string >(&pathToResultYaml))
-        ("p", boost::program_options::value < std::string >(&pathToDiagnosticsDir));
+        ("export-fixes",
+            boost::program_options::value < std::string >( &commandLineOptions.getPathToCommonYamlRef() ) )
+        ( "p",
+            boost::program_options::value < std::string >( &commandLineOptions.getPathToWorkDirRef() ) );
     const boost::program_options::parsed_options parsed =
-        boost::program_options::command_line_parser(commandLine).options(programOptions)
-        .style(boost::program_options::command_line_style::long_allow_adjacent |
-             boost::program_options::command_line_style::allow_long_disguise)
+        boost::program_options::command_line_parser( commandLine ).options( programOptions )
+        .style( boost::program_options::command_line_style::long_allow_adjacent |
+                boost::program_options::command_line_style::allow_long_disguise )
         .allow_unregistered().run();
     boost::program_options::variables_map variablesMap;
-    boost::program_options::store(parsed, variablesMap);
+    store(parsed, variablesMap);
     notify(variablesMap);
-    static std::vector < std::string > collectUnrecognized =
-        boost::program_options::collect_unrecognized(parsed.options, boost::program_options::include_positional);
-    commandLine.clear();
-    commandLine.emplace_back("--result-yaml=" + pathToResultYaml);
-    commandLine.emplace_back("--sub-linter=clang-tidy");
-    // TODO: -line-filter - also add to clazy options
-    for (size_t i = 0; i < collectUnrecognized.size(); ++i ) {
-        boost::algorithm::replace_all(collectUnrecognized[i], "\"", "\\\"");
-        std::string strInQuotes;
-        if (collectUnrecognized[i].find("-config=") == 0 ) {
-            strInQuotes = "-config";
-        }
-        if (collectUnrecognized[i].find("-line-filter=") == 0) {
-            strInQuotes = "-line-filter=";
-        }
-        if( !strInQuotes.empty() ) {
-            commandLine.emplace_back( strInQuotes + "\"" +
-                collectUnrecognized[i].substr( std::string( strInQuotes ).size(), std::string::npos ) + "\"" );
-            continue;
-        }
-        commandLine.emplace_back(collectUnrecognized[i]);
-    }
-    commandLine.emplace_back( "-p=" + pathToDiagnosticsDir);
-    commandLine.emplace_back( "--export-fixes=" + pathToDiagnosticsDir + "\\diagnosticsClangTidy.yaml");
-    commandLine.emplace_back( "--sub-linter=clazy");
-    commandLine.emplace_back( "-p=" + pathToDiagnosticsDir);
-    commandLine.emplace_back( "--export-fixes=" + pathToDiagnosticsDir + "\\diagnosticsClazy.yaml");
-    // TODO: take diagnostics from config file
-	commandLine.emplace_back( "-checks=level0,level1,level2");
-    // TODO: parse path for clazy to file from command line
-    commandLine.emplace_back( "E:\\WaveAccess\\WavePoint_General\\qt-clazy-test\\qt-clazy-test.cpp");
+    commandLineOptions.getUnrecognizedCollectionRef()
+        = collect_unrecognized(parsed.options, boost::program_options::include_positional);;
+    commandLineOptions.initCommandLine( commandLine );
 }
 
 void LintCombine::moveCommandLineToSTLContainer( stringVector & commandLine, const int argc, char ** argv ) {
--- src/LintCombineUtils.h
+++ src/LintCombineUtils.h
@@ -3,6 +3,45 @@
 #include "LinterItf.h"
 
 namespace LintCombine {
+    struct BaseLinterOptions {
+        std::vector < std::string > options;
+    };
+
+    struct ClangTidyOptions : public BaseLinterOptions {
+        explicit ClangTidyOptions(const std::string & pathToWorkDir) {
+            options.emplace_back("--sub-linter=clang-tidy");
+            options.emplace_back("-p=" + pathToWorkDir);
+            options.emplace_back("--export-fixes=" + pathToWorkDir + "\\diagnosticsClangTidy.yaml");
+        }
+    };
+
+    struct ClazyOptions : public BaseLinterOptions {
+        explicit ClazyOptions(const std::string& pathToWorkDir) {
+            options.emplace_back("--sub-linter=clazy");
+            options.emplace_back("-p=" + pathToWorkDir);
+            options.emplace_back("--export-fixes=" + pathToWorkDir + "\\diagnosticsClazy.yaml");
+        }
+    };
+
+    class CommandLineOptions {
+    public:
+        std::string& getPathToWorkDirRef() { return pathToWorkDir; }
+        std::string& getPathToCommonYamlRef() { return pathToCommonYaml; }
+        std::vector < std::string >& getUnrecognizedCollectionRef() { return unrecognizedCollection; }
+        void initCommandLine(std::vector < std::string >& commandLine);
+    private:
+        static std::string optionValueToQuotes(const std::string& optionName, const std::string& optionNameWithValue);
+        void addOptionToClangTidy(const std::string& option);
+        void addOptionToAllLinters(const std::string& option);
+        void appendLintersOptionToCommandLine(std::vector < std::string >& commandLine);
+        void initUnrecognizedOptions();
+        void initLintCombineOptions(std::vector < std::string >& commandLine) const;
+        std::vector < BaseLinterOptions* > lintersOptions;
+        std::vector < std::string > unrecognizedCollection;
+        std::string pathToCommonYaml;
+        std::string pathToWorkDir;
+    };
+
     void prepareCommandLineForReSharper( stringVector & commandLine );
 
     void moveCommandLineToSTLContainer( stringVector & commandLineSTL, int argc, char ** argv );
