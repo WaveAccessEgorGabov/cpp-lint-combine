2f47710b60b80535f4c4253b0ecdc608a8f2b3e0
Egor Gabov <egor.gabov@waveaccess.ru>
2020-06-22 22:52:40 +0300

    Added parameter clang-extra-args into run-script

--- cpp-lint-combine.cmd
+++ cpp-lint-combine.cmd
@@ -1,3 +1,6 @@
-@echo off 
-set CLAZY_CHECKS=level0,level1,level2
-cpp-lint-combine.exe -clazy-checks=%CLAZY_CHECKS% %*
+@echo off
+PATH lint-combine-build-dir\Release;%LocalAppData%\JetBrains\Installations\ReSharperPlatformVs15_689a5022_000\x86;Path\to\clazy
+set CLAZY_CHECKS_MANUAL=assert-with-side-effects,container-inside-loop,detaching-member,heap-allocated-small-trivial-type,ifndef-define-typo,inefficient-qlist,isempty-vs-count,jni-signatures,qhash-with-char-pointer-key,qproperty-type-mismatch,qrequiredresult-candidates,qstring-varargs,qt-keywords,qt4-qstring-from-array,qvariant-template-instantiation,raw-environment-function,reserve-candidates,signal-with-return-value,thread-with-slots,tr-non-literal,unneeded-cast
+set "CLAZY_CHECKS=level0,level1,%CLAZY_CHECKS_MANUAL%"
+set "CLANG_EXTRA_ARGS=-Wno-c++98-compat -Wno-missing-prototypes"
+cpp-lint-combine.exe "--clazy-checks=%CLAZY_CHECKS%" "--clang-extra-args=%CLANG_EXTRA_ARGS%" %*
