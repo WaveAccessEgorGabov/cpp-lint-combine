5fac33f4ffe068a7337220e23395afba51ba6377
Egor Gabov <egor.gabov@waveaccess.ru>
2020-06-30 14:06:00 +0300

    Updated CommandLinePreparer for TestPrepareCommandLine pass

--- src/LintCombineUtils.cpp
+++ src/LintCombineUtils.cpp
@@ -3,20 +3,55 @@
 #include <boost/program_options.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/replace.hpp>
+#include <boost/algorithm/string/join.hpp>
 #include <iostream>
-#include <fstream>
 
-LintCombine::CommandLinePreparer::CommandLinePreparer( stringVector & commandLine, std::string && toolName ) {
+LintCombine::CommandLinePreparer::CommandLinePreparer( stringVector & commandLine,
+                                                        std::string && toolName ) {
+    for( const auto & it : commandLine ) {
+        if( it == "--verbatim-commands" ) {
+            m_output.emplace_back( "Info",
+                                    "Option \"--verbatim-commands\" was set so options are passed to combine verbatim.", 0, 0 );
+            toolName.clear();
+            commandLine.erase(
+                std::remove( commandLine.begin(), commandLine.end(),
+                "--verbatim-commands" ), commandLine.end() );
+            break;
+        }
+    }
+
     boost::algorithm::to_lower( toolName );
-    if( toolName == "resharper" ) {
-        prepareCommandLineForReSharper( commandLine );
+    if( !commandLine.empty() ) {
+        if( toolName == "resharper" ) {
+            m_sourceCL = boost::algorithm::join( commandLine, " " );
+            prepareCommandLineForReSharper( commandLine );
+        }
+    }
+    else {
+        m_output.emplace_back( "Error", "Command line is empty.", 0, 0 );
+        m_isErrorWhilePrepareOccur = true;
+    }
+    printOutput();
+}
+
+LintCombine::stringVector LintCombine::CommandLinePreparer::prepareOutput() const {
+    LintCombine::stringVector preparedOutput;
+    for( const auto & it : m_output ) {
+        preparedOutput.emplace_back( it.level + ": " + it.text );
+    }
+    return preparedOutput;
+}
+
+void LintCombine::CommandLinePreparer::printOutput() const {
+    for( const auto & it : prepareOutput() ) {
+        std::cerr << it << std::endl;
     }
 }
 
 std::string LintCombine::CommandLinePreparer::optionValueToQuotes( const std::string & optionName,
-                                                                   const std::string & optionNameWithValue ) {
+                                                                    const std::string & optionNameWithValue ) {
     return optionName + "\"" +
-           optionNameWithValue.substr( optionName.size(), std::string::npos ) + "\"";
+        optionNameWithValue.substr( optionName.size(), std::string::npos ) + "\"";
 }
 
 void LintCombine::CommandLinePreparer::addOptionToClangTidy( const std::string & option ) {
@@ -40,9 +75,9 @@ void LintCombine::CommandLinePreparer::appendLintersOptionToCommandLine( stringV
     }
 }
 
-void LintCombine::CommandLinePreparer::initLintCombineOptions ( stringVector & commandLine ) const {
-    if( !m_pathToCommonYaml.empty () ) {
-        commandLine.emplace_back ( "--result-yaml=" + m_pathToCommonYaml );
+void LintCombine::CommandLinePreparer::initLintCombineOptions( stringVector & commandLine ) const {
+    if( !m_pathToGeneralYaml.empty() ) {
+        commandLine.emplace_back( "--result-yaml=" + m_pathToGeneralYaml );
     }
 }
 
@@ -52,12 +87,12 @@ void LintCombine::CommandLinePreparer::initUnrecognizedOptions() {
         boost::algorithm::replace_all( unrecognized, "\"", "\\\"" );
         std::string strToCompare = "-config=";
         if( unrecognized.find( strToCompare ) == 0 ) {
-            addOptionToClangTidy( optionValueToQuotes ( strToCompare, unrecognized ) );
+            addOptionToClangTidy( optionValueToQuotes( strToCompare, unrecognized ) );
             continue;
         }
         strToCompare = "-line-filter=";
         if( unrecognized.find( strToCompare ) == 0 ) {
-            addOptionToClangTidy( optionValueToQuotes ( strToCompare, unrecognized ) );
+            addOptionToClangTidy( optionValueToQuotes( strToCompare, unrecognized ) );
             continue;
         }
         strToCompare = "-header-filter=";
@@ -67,7 +102,7 @@ void LintCombine::CommandLinePreparer::initUnrecognizedOptions() {
         }
 
         // File to analize
-        if( unrecognized[ 0 ] != '-' && unrecognized[ 0 ] != '@' ) {
+        if( unrecognized[0] != '-' && unrecognized[0] != '@' ) {
             filesForAnalize.emplace_back( unrecognized );
             continue;
         }
@@ -80,39 +115,51 @@ void LintCombine::CommandLinePreparer::initUnrecognizedOptions() {
 }
 
 void LintCombine::CommandLinePreparer::initCommandLine( stringVector & commandLine ) {
+    // code duplication
+    if( std::find_if( std::begin( commandLine ), std::end( commandLine ),
+        []( const std::string & str ) -> bool {
+            return str.find( "clang-extra-args" ) != std::string::npos; } ) != std::end( commandLine )
+                && m_clangExtraArgs.empty() ) {
+        m_output.emplace_back( OutputRecord( "Warning", "Parameter "
+                               "\"--clang-extra-args\" was set but the parameter's "
+                               "value was not set. The parameter will be ignored.", 0, 0 ) );
+    }
+
+    if( std::find_if( std::begin( commandLine ), std::end( commandLine ),
+        []( const std::string & str ) -> bool {
+            return str.find( "clazy-checks" ) != std::string::npos; } ) != std::end( commandLine )
+                && m_clazyChecks.empty() ) {
+        m_output.emplace_back( OutputRecord( "Warning", "Parameter "
+                               "\"--clazy-checks\" was set but the parameter's "
+                               "value was not set. The parameter will be ignored.", 0, 0 ) );
+    }
+
     commandLine.clear();
-    boost::erase_all ( m_clangExtraArgs, "\"" );
-    std::istringstream iss ( m_clangExtraArgs );
-    for( auto & it : m_lintersName ) {
+    boost::erase_all( m_clangExtraArgs, "\"" );
+    std::istringstream iss( m_clangExtraArgs );
+    for( auto & it : m_lintersNames ) {
         if( it != "clang-tidy" && it != "clazy" ) {
-            std::cerr << it << " is incorrect linter name" << std::endl;
-            isErrorWhilePrepareOccur = true;
+            m_output.emplace_back( OutputRecord( "Error", "Unknown linter name: \"" + it + "\"", 0, 0 ) );
+            m_isErrorWhilePrepareOccur = true;
             continue;
         }
         if( it == "clang-tidy" ) {
-            m_lintersOptions.emplace_back ( new ClangTidyOptions ( m_pathToWorkDir ) );
+            m_lintersOptions.emplace_back( new ClangTidyOptions( m_pathToWorkDir ) );
         }
         if( it == "clazy" ) {
-            m_lintersOptions.emplace_back ( new ClazyOptions ( m_pathToWorkDir, m_clazyChecks,
-               stringVector ( std::istream_iterator<std::string> { iss },
-               std::istream_iterator<std::string> {} ) ) );
+            m_lintersOptions.emplace_back( new ClazyOptions( m_pathToWorkDir, m_clazyChecks,
+                                           stringVector( std::istream_iterator<std::string> { iss },
+                                           std::istream_iterator<std::string> {} ) ) );
         }
     }
-    if( isErrorWhilePrepareOccur ) {
+    if( m_isErrorWhilePrepareOccur ) {
         return;
     }
-    if( m_lintersOptions.empty () ) {
-        if( !m_lintersName.empty() ) {
-            std::cerr << "Linters name that was set are incorrect! "
-                    << "Used all linters by default: clang-tidy and clazy" << std::endl;
-        }
-        else {
-            std::cerr << "Linters was not set (for set linters use \"--sub-linter=\"). "
-                << "Used all linters by default: clang-tidy and clazy" << std::endl;
-        }
-        m_lintersOptions = { new ClangTidyOptions ( m_pathToWorkDir ),
-           new ClazyOptions ( m_pathToWorkDir, m_clazyChecks,
-           stringVector ( std::istream_iterator<std::string> { iss },
+    if( m_lintersOptions.empty() ) {
+        // Use all linters by default
+        m_lintersOptions = { new ClangTidyOptions( m_pathToWorkDir ),
+           new ClazyOptions( m_pathToWorkDir, m_clazyChecks,
+           stringVector( std::istream_iterator<std::string> { iss },
            std::istream_iterator<std::string> {} ) ) };
     }
     initLintCombineOptions( commandLine );
@@ -120,50 +167,95 @@ void LintCombine::CommandLinePreparer::initCommandLine( stringVector & commandLi
     appendLintersOptionToCommandLine( commandLine );
 }
 
-void LintCombine::CommandLinePreparer::prepareCommandLineForReSharper ( stringVector & commandLine ) {
-    // avoid empty value after equal sign exception
-    for( auto & it : commandLine ) {
-        if( ( it.find('-') != std::string::npos || it.find ( "--" ) != std::string::npos ) &&
-              it.find('=') == it.size() - 1 ) {
-            it.pop_back ();
+void LintCombine::CommandLinePreparer::prepareCommandLineForReSharper( stringVector & commandLine ) {
+    // TODO: remove imlicit_value and simple delete incorrect options (clazyChecks, clangExtraArgs)
+    for( size_t i = 0; i < commandLine.size(); ++i ) {
+        // find named parameter with incorrect sintax
+        if( commandLine[i].find( "-" ) == 0
+            && commandLine[i].find( "=" ) == commandLine[i].size() - 1 ) {
+            // if parameter is clazy-checks or clang-extra-args - Ignore them.
+            if( commandLine[i].find( "clazy-checks" ) != std::string::npos
+                || commandLine[i].find( "clang-extra-args" ) != std::string::npos ) {
+                m_output.emplace_back(
+                    OutputRecord( "Warning", "Parameter \"" +
+                    commandLine[i] + "\" was set but the parameter's value was not set. "
+                    "The parameter will be ignored.", 0, 0 ) );
+                commandLine.erase( std::begin( commandLine ) + i );
+                --i;
+            }
+            else {
+                m_output.emplace_back(
+                    OutputRecord( "Error", "Value of parameter \"" +
+                    commandLine[i] + "\" must follow after the equal sign", 0, 0 ) );
+                m_isErrorWhilePrepareOccur = true;
+                commandLine.erase( std::begin( commandLine ) + i );
+                --i;
+            }
+            continue;
+        }
+
+        // --sub-linter=<added to m_lintersName>
+        if( commandLine[i].find( "--sub-linter=" ) == 0 ) {
+            m_lintersNames.emplace_back( commandLine[i].substr(
+                std::string( "--sub-linter=" ).size(), commandLine[i].size() ) );
+            commandLine.erase( std::begin( commandLine ) + i );
+            --i;
+            continue;
+        }
+
+        // --sub-linter <added to m_lintersName>
+        if( commandLine[i].find( "--sub-linter" ) == 0 ) {
+            if( i < commandLine.size() - 1 ) {
+                m_lintersNames.emplace_back( commandLine[i + 1] );
+            }
+            else {
+                m_lintersNames.emplace_back( std::string() );
+            }
+            commandLine.erase( std::begin( commandLine ) + i );
+            --i;
         }
     }
 
     boost::program_options::options_description programOptions;
-    programOptions.add_options ()
-        ( "verbatim-commands", "pass options verbatim" )
-        ( "sub-linter",
-          boost::program_options::value < std::vector < std::string > > ( &m_lintersName ) )
+    programOptions.add_options()
         ( "clazy-checks",
-          boost::program_options::value < std::string > ( &m_clazyChecks )->
-                implicit_value( std::string() ) )
+          boost::program_options::value < std::string >( &m_clazyChecks )
+          ->implicit_value( std::string() ) )
         ( "clang-extra-args",
-          boost::program_options::value < std::string > ( &m_clangExtraArgs )->
-                implicit_value( std::string () ) )
+          boost::program_options::value < std::string >( &m_clangExtraArgs )
+          ->implicit_value( std::string() ) )
         ( "export-fixes",
-          boost::program_options::value < std::string > ( &m_pathToCommonYaml ) )
+          boost::program_options::value < std::string >( &m_pathToGeneralYaml ) )
         ( "p",
-          boost::program_options::value < std::string > ( &m_pathToWorkDir ) );
+          boost::program_options::value < std::string >( &m_pathToWorkDir ) );
     boost::program_options::variables_map variablesMap;
     const boost::program_options::parsed_options parsed =
-        boost::program_options::command_line_parser ( commandLine ).options ( programOptions )
-        .style ( boost::program_options::command_line_style::default_style |
+        boost::program_options::command_line_parser( commandLine ).options( programOptions )
+        .style( boost::program_options::command_line_style::default_style |
                  boost::program_options::command_line_style::allow_long_disguise )
-        .allow_unregistered ().run ();
-    store ( parsed, variablesMap );
-    notify ( variablesMap );
+        .allow_unregistered().run();
+    store( parsed, variablesMap );
+    notify( variablesMap );
     m_unrecognizedCollection =
-        collect_unrecognized ( parsed.options, boost::program_options::include_positional );
-    if( variablesMap.count ( "verbatim-commands" ) ) {
+        collect_unrecognized( parsed.options, boost::program_options::include_positional );
+    // TODO: separate function for data validation here
+    if( m_pathToWorkDir.empty() ) {
+        m_output.emplace_back( "Error", "Path to compilation database is empty.", 0, 0 );
+        m_isErrorWhilePrepareOccur = true;
         return;
     }
-    initCommandLine ( commandLine );
+    if( m_pathToGeneralYaml.empty() ) {
+        m_output.emplace_back( "Error", "Path to yaml-file is empty.", 0, 0 );
+        m_isErrorWhilePrepareOccur = true;
+        return;
+    }
+    initCommandLine( commandLine );
 }
 
 LintCombine::stringVector LintCombine::moveCommandLineToSTLContainer( const int argc, char ** argv ) {
     stringVector commandLine;
     for( auto i = 1; i < argc; ++i ) {
-        commandLine.emplace_back( std::string( argv[ i ] ) );
+        commandLine.emplace_back( std::string( argv[i] ) );
     }
     return commandLine;
 }
--- src/LintCombineUtils.h
+++ src/LintCombineUtils.h
@@ -3,53 +3,67 @@
 #include "LinterItf.h"
 
 namespace LintCombine {
-    struct BaseLinterOptions {
-        std::string linterName;
-        stringVector options;
-    };
-
-    struct ClangTidyOptions : BaseLinterOptions {
-        explicit ClangTidyOptions( const std::string & pathToWorkDir ) {
-            linterName = "clang-tidy";
-            options.emplace_back( "--sub-linter=clang-tidy" );
-            if( !pathToWorkDir.empty () ) {
-                options.emplace_back ( "-p=" + pathToWorkDir );
-                options.emplace_back ( "--export-fixes=" + pathToWorkDir + "\\diagnosticsClangTidy.yaml" );
-            }
-        }
-    };
-
-    struct ClazyOptions : BaseLinterOptions {
-        explicit ClazyOptions( const std::string & pathToWorkDir,
-                               const std::string & checks,
-                               std::vector < std::string > && clangExtraArgs ) {
-            linterName = "clazy";
-            options.emplace_back( "--sub-linter=clazy" );
-            if( !pathToWorkDir.empty () ) {
-                options.emplace_back ( "-p=" + pathToWorkDir );
-                options.emplace_back ( "--export-fixes=" + pathToWorkDir + "\\diagnosticsClazy.yaml" );
-            }
-            if( !checks.empty () && checks.find("--") == std::string::npos ) {
-                options.emplace_back ( "--checks=" + checks );
-            }
-            if( !clangExtraArgs.empty () ) {
-                for( const auto & it : clangExtraArgs ) {
-                    if( !it.empty () && it.find("--") == std::string::npos ) {
-                        options.emplace_back ( "--extra-arg=" + it );
-                    }
-                }
-            }
-        }
-    };
 
     class CommandLinePreparer {
 
     public:
         CommandLinePreparer( stringVector & commandLine, std::string && toolName );
 
-        bool getIsErrorWhilePrepareOccur () const { return isErrorWhilePrepareOccur; }
+        bool getIsErrorWhilePrepareOccur () const { return m_isErrorWhilePrepareOccur; }
 
     private:
+        struct OutputRecord {
+            std::string level;
+            std::string text;
+            unsigned int startPosInCL;
+            unsigned int endPosInCL;
+
+            OutputRecord( const std::string & levelInit, const std::string & textInit,
+                          const unsigned int startPosInCLInit,
+                          const unsigned int endPosInCLInit )
+                : level( levelInit ), text( textInit ),
+                  startPosInCL( startPosInCLInit ), endPosInCL( endPosInCLInit ) {}
+        };
+
+        struct BaseLinterOptions {
+            std::string linterName;
+            stringVector options;
+        };
+
+        struct ClangTidyOptions : BaseLinterOptions {
+            explicit ClangTidyOptions ( const std::string & pathToWorkDir ) {
+                linterName = "clang-tidy";
+                options.emplace_back ( "--sub-linter=clang-tidy" );
+                if( !pathToWorkDir.empty () ) {
+                    options.emplace_back ( "-p=" + pathToWorkDir );
+                    options.emplace_back ( "--export-fixes=" + pathToWorkDir + "\\diagnosticsClangTidy.yaml" );
+                }
+            }
+        };
+
+        struct ClazyOptions : BaseLinterOptions {
+            explicit ClazyOptions ( const std::string & pathToWorkDir,
+                                    const std::string & checks,
+                                    std::vector < std::string > && clangExtraArgs ) {
+                linterName = "clazy";
+                options.emplace_back ( "--sub-linter=clazy" );
+                if( !pathToWorkDir.empty () ) {
+                    options.emplace_back ( "-p=" + pathToWorkDir );
+                    options.emplace_back ( "--export-fixes=" + pathToWorkDir + "\\diagnosticsClazy.yaml" );
+                }
+                if( !checks.empty () && checks.find ( "--" ) == std::string::npos ) {
+                    options.emplace_back ( "--checks=" + checks );
+                }
+                if( !clangExtraArgs.empty () ) {
+                    for( const auto & it : clangExtraArgs ) {
+                        if( !it.empty () && it.find ( "--" ) == std::string::npos ) {
+                            options.emplace_back ( "--extra-arg=" + it );
+                        }
+                    }
+                }
+            }
+        };
+
         void prepareCommandLineForReSharper( stringVector & commandLine );
 
         static std::string
@@ -67,14 +81,20 @@ namespace LintCombine {
 
         void initLintCombineOptions( stringVector & commandLine ) const;
 
+        stringVector prepareOutput() const;
+
+        void printOutput() const;
+
         std::vector < BaseLinterOptions * > m_lintersOptions;
+        std::vector < OutputRecord > m_output;
         stringVector m_unrecognizedCollection;
-        stringVector m_lintersName;
-        std::string m_pathToCommonYaml;
+        stringVector m_lintersNames;
+        std::string m_pathToGeneralYaml;
         std::string m_pathToWorkDir;
         std::string m_clazyChecks;
         std::string m_clangExtraArgs;
-        bool isErrorWhilePrepareOccur = false;
+        std::string m_sourceCL;
+        bool m_isErrorWhilePrepareOccur = false;
     };
 
     stringVector moveCommandLineToSTLContainer( int argc, char ** argv );
