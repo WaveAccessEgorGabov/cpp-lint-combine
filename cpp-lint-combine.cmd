b219fcaddddf95c8356df800be4f2e9b6efbc7aa
Egor Gabov <egor.gabov@waveaccess.ru>
2020-07-08 13:58:35 +0300

    Now script-trampoline call cpp-lint-combine with "--ide-profile=resharper"

--- cpp-lint-combine.cmd
+++ cpp-lint-combine.cmd
@@ -4,4 +4,4 @@ PATH lint-combine-build-dir\Release;%RESHARPER_DIR%\x86;%RESHARPER_DIR%_000\x86;
 set CLAZY_CHECKS_MANUAL=assert-with-side-effects,container-inside-loop,detaching-member,heap-allocated-small-trivial-type,ifndef-define-typo,inefficient-qlist,isempty-vs-count,jni-signatures,qhash-with-char-pointer-key,qproperty-type-mismatch,qrequiredresult-candidates,qstring-varargs,qt-keywords,qt4-qstring-from-array,qvariant-template-instantiation,raw-environment-function,reserve-candidates,signal-with-return-value,thread-with-slots,tr-non-literal,unneeded-cast
 set "CLAZY_CHECKS=level0,level1,level2,%CLAZY_CHECKS_MANUAL%"
 set "CLANG_EXTRA_ARGS=-w"
-cpp-lint-combine.exe "--clazy-checks=%CLAZY_CHECKS%" "--clang-extra-args=%CLANG_EXTRA_ARGS%" %*
+cpp-lint-combine.exe "--ide-profile=resharper" "--clazy-checks=%CLAZY_CHECKS%" "--clang-extra-args=%CLANG_EXTRA_ARGS%" %*
