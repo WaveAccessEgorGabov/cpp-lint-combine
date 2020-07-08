4b600ff23d60dc4938453ee5fb5c3a1b3803d632
Egor Gabov <egor.gabov@waveaccess.ru>
2020-07-08 10:39:14 +0300

    Updated script-trampoline
    RESHARPER_DIR can changed => let's find clang-tidy in RESHARPER_DIR_000
    and in RESHARPER_DIR

--- cpp-lint-combine.cmd
+++ cpp-lint-combine.cmd
@@ -1,6 +1,7 @@
 @echo off
-PATH lint-combine-build-dir\Release;%LocalAppData%\JetBrains\Installations\ReSharperPlatformVs15_689a5022_000\x86;Path\to\clazy
+set "RESHARPER_DIR=%LocalAppData%\JetBrains\Installations\ReSharperPlatformVs15_689a5022"
+PATH lint-combine-build-dir\Release;%RESHARPER_DIR%\x86;%RESHARPER_DIR%_000\x86;Path\to\clazy
 set CLAZY_CHECKS_MANUAL=assert-with-side-effects,container-inside-loop,detaching-member,heap-allocated-small-trivial-type,ifndef-define-typo,inefficient-qlist,isempty-vs-count,jni-signatures,qhash-with-char-pointer-key,qproperty-type-mismatch,qrequiredresult-candidates,qstring-varargs,qt-keywords,qt4-qstring-from-array,qvariant-template-instantiation,raw-environment-function,reserve-candidates,signal-with-return-value,thread-with-slots,tr-non-literal,unneeded-cast
-set "CLANG_EXTRA_ARGS=-Wno-c++98-compat -Wno-reserved-id-macro -Wno-comma"
-set "CLAZY_CHECKS=level0,level1,%CLAZY_CHECKS_MANUAL%"
+set "CLAZY_CHECKS=level0,level1,level2,%CLAZY_CHECKS_MANUAL%"
+set "CLANG_EXTRA_ARGS=-w"
 cpp-lint-combine.exe "--clazy-checks=%CLAZY_CHECKS%" "--clang-extra-args=%CLANG_EXTRA_ARGS%" %*
