952d1bb3e5bc7bb7d89c384a72dc0d781c4c1c2a
Egor Gabov <Egor.Gabov@waveaccess.ru>
2020-07-22 16:35:44 +0300

    Added macros undef
    For correct work cpp-lint-combine in CLion we must undefined some macros

--- cpp-lint-combine.sh
+++ cpp-lint-combine.sh
@@ -1,15 +1,5 @@
-#DONT WORK WITHOUT THIS
-echo "" > /tmp/clion-clang-tidy/macros
-echo "" > /tmp/clion-clang-tidy1/macros
-echo "" > /tmp/clion-clang-tidy2/macros
-echo "" > /tmp/clion-clang-tidy3/macros
-echo "" > /tmp/clion-clang-tidy4/macros
-echo "" > /tmp/clion-clang-tidy5/macros
-echo "" > /tmp/clion-clang-tidy6/macros
-
 CLAZY_CHECKS_MANUAL=assert-with-side-effects,container-inside-loop,detaching-member,heap-allocated-small-trivial-type,ifndef-define-typo,inefficient-qlist,isempty-vs-count,jni-signatures,qhash-with-char-pointer-key,qproperty-type-mismatch,qrequiredresult-candidates,qstring-varargs,qt-keywords,qt4-qstring-from-array,qt6-qlatin1string-to-u,qvariant-template-instantiation,raw-environment-function,reserve-candidates,signal-with-return-value,thread-with-slots,tr-non-literal,unneeded-cast
 CLAZY_CHECKS="level0,level1,level2,$CLAZY_CHECKS_MANUAL"
 CLANG_EXTRA_ARGS="-w"
 
-#LOG_FILE="/path/to/log/file.txt"
-cpp-lint-combine "--ide-profile=CLion" "--clazy-checks=$CLAZY_CHECKS" "--clang-extra-args=$CLANG_EXTRA_ARGS" $@ #2>>$LOG_FILE 1>>$LOG_FILE
+cpp-lint-combine "--ide-profile=CLion" "--clazy-checks=$CLAZY_CHECKS" "--clang-extra-args=$CLANG_EXTRA_ARGS" $@
--- src/PrepareCmdLineCLion.cpp
+++ src/PrepareCmdLineCLion.cpp
@@ -1,5 +1,8 @@
 #include "PrepareCmdLineCLion.h"
 
+#include <fstream>
+#include <sstream>
+
 void LintCombine::PrepareCmdLineCLion::initOptionsToSpecificIDE() {
     stringVector filesForAnalize;
     for( auto & unrecognized : m_unrecognizedCollection ) {
@@ -14,4 +17,23 @@ void LintCombine::PrepareCmdLineCLion::initOptionsToSpecificIDE() {
     for( const auto & it : filesForAnalize ) {
         addOptionToAllLinters( it );
     }
+
+    std::ifstream sourceMacrosFile( m_pathToWorkDir + "/macros" );
+    std::ostringstream pBuf;
+    pBuf << sourceMacrosFile.rdbuf();
+    std::string buf = pBuf.str();
+    // The following macros must be undefined to avoid redefinition,
+    // because clazy also defines this macros
+    std::ofstream updatedMacrosFile( m_pathToWorkDir + "/macros" );
+    updatedMacrosFile << "#undef __clang_version__\n";
+    updatedMacrosFile << "#undef __VERSION__\n";
+    updatedMacrosFile << "#undef __has_feature\n";
+    updatedMacrosFile << "#undef __has_extension\n";
+    updatedMacrosFile << "#undef __has_attribute\n";
+    updatedMacrosFile << "#undef __has_builtin\n";
+    updatedMacrosFile << buf;
+    // clang in not compatible with GCC 8 or higher,
+    // so we can't use the following macros
+    updatedMacrosFile << "#define _GLIBCXX_USE_MAKE_INTEGER_SEQ 1\n";
+    updatedMacrosFile << "#undef __builtin_va_start\n";
 }
