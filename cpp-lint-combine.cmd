a88d3759b4a33d0d09d3fdf2e08d9472d595bd0e
Egor Gabov <egor.gabov@waveaccess.ru>
2020-07-24 18:21:00 +0300

    Unify bootstrap script for using in the CLion and in the ReSharper

--- cpp-lint-combine.cmd
+++ cpp-lint-combine.cmd
@@ -1,7 +1,30 @@
 @echo off
-set "RESHARPER_DIR=%LocalAppData%\JetBrains\Installations\ReSharperPlatformVs15_689a5022"
-PATH lint-combine-build-dir\Release;%RESHARPER_DIR%\x86;%RESHARPER_DIR%_000\x86;Path\to\clazy
-set CLAZY_CHECKS_MANUAL=assert-with-side-effects,container-inside-loop,detaching-member,heap-allocated-small-trivial-type,ifndef-define-typo,inefficient-qlist,isempty-vs-count,jni-signatures,qhash-with-char-pointer-key,qproperty-type-mismatch,qrequiredresult-candidates,qstring-varargs,qt-keywords,qt4-qstring-from-array,qt6-qlatin1string-to-u,qvariant-template-instantiation,raw-environment-function,reserve-candidates,signal-with-return-value,thread-with-slots,tr-non-literal,unneeded-cast
-set "CLAZY_CHECKS=level0,level1,level2,%CLAZY_CHECKS_MANUAL%"
-set "CLANG_EXTRA_ARGS=-w"
-cpp-lint-combine.exe "--ide-profile=resharper" "--clazy-checks=%CLAZY_CHECKS%" "--clang-extra-args=%CLANG_EXTRA_ARGS%" %*
+:: Set the name of the used IDE
+set "IDE_PROFILE=<chooce one from list: CLION, RESHARPER>" 
+
+:: Set paths to IDEs. If you doesn't have some of this IDE - ignore it
+set CLION_CLANG_TIDY_PATH=<Path to the directory with CLion's clang-tidy>
+:: Directory with ReSharper can be changed
+set RESHARPER_DIR=<Path ReSharper's dir>
+set RESHARPER_CLANG_TIDY_PATH=%RESHARPER_DIR%\x86;%RESHARPER_DIR%_000\x86
+ 
+:: Set path to clazy
+set CLAZY_PATH=<Path to the directory with clazy>
+
+:: Set path to cpp-lint-combine
+set CPP_LINT_COMBINE_PATH=<Path to the directory with cpp-lint-combine executable>
+
+set IDE_CLANG_TIDY_PATH=%IDE_PROFILE%%_CLANG_TIDY_PATH%
+PATH %CLAZY_PATH%;%IDE_CLANG_TIDY_PATH%;%CPP_LINT_COMBINE_PATH%
+
+:: Set clazy checks
+set CLAZY_CHECKS_MANUAL="assert-with-side-effects,container-inside-loop,detaching-member,heap-allocated-small-trivial-type,ifndef-define-typo,inefficient-qlist,isempty-vs-count,jni-signatures,qhash-with-char-pointer-key,qproperty-type-mismatch,qrequiredresult-candidates,qstring-varargs,qt-keywords,qt4-qstring-from-array,qvariant-template-instantiation,raw-environment-function,reserve-candidates,signal-with-return-value,thread-with-slots,tr-non-literal,unneeded-cast"
+set CLAZY_CHECKS="level0,level1,level2,%CLAZY_CHECKS_MANUAL%"
+
+:: Set clang extra args
+set CLANG_EXTRA_ARGS="-w"
+
+:: You can add argument "--sub-linter" to the cpp-lint-combine's command line 
+:: with value from list: clang-tidy, clazy (e.g. --sub-linter=clang-tidy)
+:: By default all supported linters used
+cpp-lint-combine.exe "--ide-profile=%IDE_PROFILE%" "--clazy-checks=%CLAZY_CHECKS%" "--clang-extra-args=%CLANG_EXTRA_ARGS%" %*
--- src/PrepareCmdLineCLion.cpp
+++ src/PrepareCmdLineCLion.cpp
@@ -1,11 +1,16 @@
 #include "PrepareCmdLineCLion.h"
 
+#include <boost/algorithm/string/replace.hpp>
+
 #include <fstream>
 #include <sstream>
 
 void LintCombine::PrepareCmdLineCLion::actionsForSpecificIDE() {
     stringVector filesForAnalize;
     for( auto & unrecognized : m_unrecognizedCollection ) {
+        if constexpr( BOOST_OS_LINUX ) {
+            boost::algorithm::replace_all( unrecognized, "\"", "\\\"" );
+        }
         // File to analize
         if( unrecognized[0] != '-' && unrecognized[0] != '@' ) {
             filesForAnalize.emplace_back( unrecognized );
@@ -22,18 +27,24 @@ void LintCombine::PrepareCmdLineCLion::actionsForSpecificIDE() {
     std::ostringstream pBuf;
     pBuf << sourceMacrosFile.rdbuf();
     std::string buf = pBuf.str();
-    // The following macros must be undefined to avoid redefinition,
-    // because clazy also defines this macros
     std::ofstream updatedMacrosFile( m_pathToWorkDir + "/macros" );
-    updatedMacrosFile << "#undef __clang_version__\n";
-    updatedMacrosFile << "#undef __VERSION__\n";
-    updatedMacrosFile << "#undef __has_feature\n";
-    updatedMacrosFile << "#undef __has_extension\n";
-    updatedMacrosFile << "#undef __has_attribute\n";
-    updatedMacrosFile << "#undef __has_builtin\n";
+
+    // In Linux the following macros must be undefined to avoid redefinition,
+    // because clazy also defines this macros
+    if constexpr( BOOST_OS_LINUX ) {
+        updatedMacrosFile << "#undef __clang_version__\n";
+        updatedMacrosFile << "#undef __VERSION__\n";
+        updatedMacrosFile << "#undef __has_feature\n";
+        updatedMacrosFile << "#undef __has_extension\n";
+        updatedMacrosFile << "#undef __has_attribute\n";
+        updatedMacrosFile << "#undef __has_builtin\n";
+    }
     updatedMacrosFile << buf;
-    // clang in not compatible with GCC 8 or higher,
+
+    // In Linux clang in not compatible with GCC 8 or higher,
     // so we can't use the following macros
-    updatedMacrosFile << "#define _GLIBCXX_USE_MAKE_INTEGER_SEQ 1\n";
-    updatedMacrosFile << "#undef __builtin_va_start\n";
+    if constexpr( BOOST_OS_LINUX ) {
+        updatedMacrosFile << "#define _GLIBCXX_USE_MAKE_INTEGER_SEQ 1\n";
+        updatedMacrosFile << "#undef __builtin_va_start\n";
+    }
 }
