a5da96791d90a4010910b0f216a478d3332a01b9
Egor Gabov <egor.gabov@waveaccess.ru>
2020-07-27 16:52:42 +0300

    Unify bootstrap-script for both OS

--- cpp-lint-combine.sh
+++ cpp-lint-combine.sh
@@ -1,20 +1,27 @@
 # Set the name of the used IDE
-set "IDE_PROFILE=<chooce one from list: CLION, RESHARPER>"
+set IDE_PROFILE="<chooce one from list: CLION, RESHARPER>"
 
-# Set paths to IDEs clang-tidy. If you doesn't have some of this IDE - ignore it
-set CLION_CLANG_TIDY_PATH=<Path to the directory with CLions clang-tidy>
+# Set paths directory with IDEs clang-tidy. If you doesn't have some of this IDE - ignore it
+
+# Windows examle path: C:\Program Files\JetBrains\CLion <version>\bin\clang\win
+set CLION_CLANG_TIDY_PATH="<Path to the directory with CLions clang-tidy>"
+
+# Directory with ReSharper can be changed from version to version.
+# Windows example path: %LocalAppData%\JetBrains\Installations\ReSharperPlatformVs15_689a5022
+set RESHARPER_DIR="<Path ReSharper's dir>"
+set RESHARPER_CLANG_TIDY_PATH="%RESHARPER_DIR%\x86;%RESHARPER_DIR%_000\x86"
 
 # Set path to clazy
-set CLAZY_PATH=<Path to the directory with clazy>
+set CLAZY_PATH="<Path to the directory with clazy-standalone>"
 
 # Set path to cpp-lint-combine
-set CPP_LINT_COMBINE_PATH=<Path to the directory with cpp-lint-combine executable>
+set CPP_LINT_COMBINE_PATH="<Path to the directory with cpp-lint-combine executable>"
 
-set IDE_CLANG_TIDY_PATH=%IDE_PROFILE%%_CLANG_TIDY_PATH%
-#PATH=$CLAZY_PATH;$IDE_CLANG_TIDY_PATH;$CPP_LINT_COMBINE_PATH
+set IDE_CLANG_TIDY_PATH="%IDE_PROFILE%%_CLANG_TIDY_PATH%"
+PATH=$CLAZY_PATH;$IDE_CLANG_TIDY_PATH;$CPP_LINT_COMBINE_PATH
 
 # Set clazy checks
-CLAZY_CHECKS_MANUAL=assert-with-side-effects,container-inside-loop,detaching-member,heap-allocated-small-trivial-type,ifndef-define-typo,inefficient-qlist,isempty-vs-count,jni-signatures,qhash-with-char-pointer-key,qproperty-type-mismatch,qrequiredresult-candidates,qstring-varargs,qt-keywords,qt4-qstring-from-array,qt6-qlatin1string-to-u,qvariant-template-instantiation,raw-environment-function,reserve-candidates,signal-with-return-value,thread-with-slots,tr-non-literal,unneeded-cast
+CLAZY_CHECKS_MANUAL="assert-with-side-effects,container-inside-loop,detaching-member,heap-allocated-small-trivial-type,ifndef-define-typo,inefficient-qlist,isempty-vs-count,jni-signatures,qhash-with-char-pointer-key,qproperty-type-mismatch,qrequiredresult-candidates,qstring-varargs,qt-keywords,qt4-qstring-from-array,qt6-qlatin1string-to-u,qvariant-template-instantiation,raw-environment-function,reserve-candidates,signal-with-return-value,thread-with-slots,tr-non-literal,unneeded-cast"
 CLAZY_CHECKS="level0,level1,level2,$CLAZY_CHECKS_MANUAL"
 
 #Set clang extra args
