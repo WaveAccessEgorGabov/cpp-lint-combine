db7386447d1ff0eddce5aa342f704897e788434f
Egor Gabov <egor.gabov@waveaccess.ru>
2020-07-29 16:54:32 +0300

    Unused code from LintCombineUtils deleted.

--- src/LintCombineUtils.cpp
+++ src/LintCombineUtils.cpp
@@ -1,8 +1,5 @@
 #include "LintCombineUtils.h"
 
-#include <iostream>
-
-#include <boost/algorithm/string/join.hpp>
 #include <boost/program_options.hpp>
 
 LintCombine::stringVector LintCombine::cmdLineToSTLContainer( const int argc, char ** argv ) {
@@ -40,38 +37,5 @@ void LintCombine::fixHyphensInCmdLine( stringVector & cmdLine ) {
                 it.erase( it.begin() );
             }
         }
-    }
-}
-
-LintCombine::stringVector
-LintCombine::prepareDiagnostics( const stringVector & cmdLine,
-                                 const std::vector< Diagnostic > & diagnostics ) {
-    const auto sourceCL = boost::algorithm::join( cmdLine, " " );
-    stringVector preparedOutput;
-    for( const auto & it : diagnostics ) {
-        std::string levelAsString;
-        switch( it.level ) {
-            case Level::Trace:   { levelAsString = "Trace";   break; }
-            case Level::Debug:   { levelAsString = "Debug";   break; }
-            case Level::Info:    { levelAsString = "Info";    break; }
-            case Level::Warning: { levelAsString = "Warning"; break; }
-            case Level::Error:   { levelAsString = "Error";   break; }
-            case Level::Fatal:   { levelAsString = "Fatal";   break; }
-        }
-        preparedOutput.emplace_back( levelAsString + ": " + it.text );
-        if( it.firstPos < it.lastPos ) {
-            std::string errorShow = sourceCL + "\n";
-            errorShow.append( it.firstPos, ' ' );
-            errorShow.append( it.lastPos - it.firstPos, '~' );
-            preparedOutput.emplace_back( errorShow );
-        }
-    }
-    return preparedOutput;
-}
-
-void LintCombine::printDiagnostics( const stringVector & cmdLine,
-                                    const std::vector< Diagnostic > & diagnostics ) {
-    for( const auto & it : prepareDiagnostics( cmdLine, diagnostics ) ) {
-        std::cerr << it << std::endl;
     }
 }
--- src/LintCombineUtils.h
+++ src/LintCombineUtils.h
@@ -6,12 +6,6 @@ namespace LintCombine {
     stringVector cmdLineToSTLContainer( int argc, char ** argv );
 
     void fixHyphensInCmdLine( stringVector & cmdLine );
-
-    static stringVector prepareDiagnostics( const stringVector & cmdLine,
-                                     const std::vector< Diagnostic > & diagnostics );
-
-    void printDiagnostics( const stringVector & cmdLine,
-                           const std::vector< Diagnostic > & diagnostics );
 }
 
 
