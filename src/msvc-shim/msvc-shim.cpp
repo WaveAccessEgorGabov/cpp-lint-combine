e158c999b35ac7c77d3b3b2aa06e968f9c9f47a2
EgorGabov <Egor.Gabov@waveaccess.ru>
2020-10-09 17:07:24 +0300

    Now combine's bootstrap-script is called from msvc-ship with options.

--- src/msvc-shim/msvc-shim.cpp
+++ src/msvc-shim/msvc-shim.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
+#include <string>
 
-int main() {
-    return std::system( PATH_TO_COMBINES_BOOTSTRAP );
+int main( int argc, char ** argv ) {
+    std::string cmdLine = PATH_TO_COMBINES_BOOTSTRAP;
+    for( int i = 1; i < argc; cmdLine += " ", cmdLine += argv[i++] );
+    return std::system( cmdLine.c_str() );
 }
-
