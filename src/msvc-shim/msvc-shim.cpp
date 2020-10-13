83807e3e93adaa246eda5a8577de8b97b434f3d8
EgorGabov <Egor.Gabov@waveaccess.ru>
2020-10-13 13:56:38 +0300

    msvc-shim now pass path to origin clang-tidy into bootstrap-script

--- src/msvc-shim/msvc-shim.cpp
+++ src/msvc-shim/msvc-shim.cpp
@@ -1,8 +1,10 @@
-#include <cstdlib>
 #include <string>
+#include <filesystem>
 
 int main( int argc, char ** argv ) {
-    std::string cmdLine = PATH_TO_COMBINES_BOOTSTRAP;
-    for( int i = 1; i < argc; cmdLine += " ", cmdLine += argv[i++] );
-    return std::system( cmdLine.c_str() );
+    std::string childProcessCmdLine =
+        PATH_TO_COMBINES_BOOTSTRAP " " +
+        std::filesystem::path( argv[0] ).parent_path().string() + "/clang-tidy/clang-tidy.exe";
+    for( int i = 1; i < argc; childProcessCmdLine += " " + std::string( argv[i++] ) );
+    return std::system( childProcessCmdLine.c_str() );
 }
