d1bb5ec974931ed37d257e9b2000d001f8b1fcf8
EgorGabov <Egor.Gabov@waveaccess.ru>
2020-10-27 18:47:21 +0300

    LOFCTest: New lines added to the linter's output

--- test/mockPrograms/mockForLOFTest.sh
+++ test/mockPrograms/mockForLOFTest.sh
@@ -13,14 +13,19 @@ do
 done
 
 fillBuffer() {
-    printf '%*s' $1 #| tr ' ' 'a' # for debug
+    printf '%*s' $1
 }
 
+# $1 - Linter output size
+# $2 - Linter output
+# $3 - Converted linter output
+# $4 - Print linter output or expected result
+# $5 - Number of new lines (\n) in the linter output
 generateStringForDifferentBufferSize() {
-    for i in {256..8192..256} # TODO: Think about buffer size and step
+    for i in {256..2048}
     do
         spaceFromBuffersBeginToMessage=$(($i-$1))
-        spaceFromMessageToBuffersEnd=$(($i-$(($(($i-$1+${#2})) % $i))))
+        spaceFromMessageToBuffersEnd=$(($i+$5-$(($(($i-$1+${#2})) % $i))))
 
         fillBuffer $spaceFromBuffersBeginToMessage
         if $4; then
@@ -43,111 +48,113 @@ rowColumnSource="(42,42):"
 rowColumnConverted=":42:42:"
 checkNameSource="[-wsome-check]"
 checkNameConverted="[wsome-check]"
-testStrSource="${pathToFile}${rowColumnSource}${commonCheckPart}${checkNameSource}"
-testStrConverted="${pathToFile}${rowColumnConverted}${commonCheckPart}${checkNameConverted}"
+testStrSource="${pathToFile}${rowColumnSource}${commonCheckPart}${checkNameSource}\n"
+testStrConverted="${pathToFile}${rowColumnConverted}${commonCheckPart}${checkNameConverted}\n"
+newLineSymSize=2
 
 # Test 1 - Check's message on the border of buffer
-generateStringForDifferentBufferSize $((${#testStrSource})) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+generateStringForDifferentBufferSize $((${#testStrSource} - ${newLineSymSize})) \
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Test 2 - Check's message split between buffers in the following way:
 # <some_text>[wsome-check
 # ]<some_text>
-generateStringForDifferentBufferSize $((${#testStrSource} - 1)) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+generateStringForDifferentBufferSize $((${#testStrSource} - ${newLineSymSize} - 1)) \
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Test 3 - Check's message split between buffers in the following way:
 # <some_text>[wsome
 # -check]<some_text>
-generateStringForDifferentBufferSize $((${#testStrSource} - ${#checkNameSource} / 2)) \
-                                     "$testStrSource" "$testStrConverted" $getTests
-
+generateStringForDifferentBufferSize $((${#testStrSource} - ${newLineSymSize} - ${#checkNameSource} / 2)) \
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 # Test 4 - Check's message split between buffers in the following way:
 # <some_text>[
 # wsome-check]<some_text>
-generateStringForDifferentBufferSize $((${#testStrSource} - ${#checkNameSource} + 1)) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+generateStringForDifferentBufferSize $((${#testStrSource} - ${newLineSymSize} - ${#checkNameSource} + 1)) \
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Test 5 - Check's message split between buffers in the following way:
 # <some_text>
 # [wsome-check]<some_text>
-generateStringForDifferentBufferSize $((${#testStrSource} - ${#checkNameSource})) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+generateStringForDifferentBufferSize $((${#testStrSource} - ${newLineSymSize} - ${#checkNameSource})) \
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Test 6 - Check's message split between buffers in the following way:
 # <some_text>mai(42,42)n.cpp(42,42)
 # <some_text>: warning
-generateStringForDifferentBufferSize $((${#testStrSource} - ${#checkNameSource} - ${#commonCheckPart} - 1)) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+generateStringForDifferentBufferSize $((${#testStrSource} - ${newLineSymSize} - \
+                                     ${#checkNameSource} - ${#commonCheckPart} - 1)) \
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Test 7 - Check's message split between buffers in the following way:
 # <some_text>mai(42,42)n.cpp(42
 # <some_text>,42): warning
-generateStringForDifferentBufferSize $((${#testStrSource} - ${#checkNameSource} - \
+generateStringForDifferentBufferSize $((${#testStrSource} - ${newLineSymSize} - ${#checkNameSource} - \
                                      ${#commonCheckPart} - ${#rowColumnSource} / 2 - 1)) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Test 8 - Check's message split between buffers in the following way:
 # <some_text>mai(42,42)n.cpp(
 # <some_text>42,42): warning
-generateStringForDifferentBufferSize $((${#testStrSource} - ${#checkNameSource} - \
+generateStringForDifferentBufferSize $((${#testStrSource} - ${newLineSymSize} - ${#checkNameSource} - \
                                      ${#commonCheckPart} - ${#rowColumnSource} + 1)) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Test 9 - Check's message split between buffers in the following way:
 # <some_text>mai(42,42)n.cpp
 # <some_text>(42,42): warning
-generateStringForDifferentBufferSize $((${#testStrSource} - ${#checkNameSource} - \
+generateStringForDifferentBufferSize $((${#testStrSource} - ${newLineSymSize} - ${#checkNameSource} - \
                                      ${#commonCheckPart} - ${#rowColumnSource})) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Test 10 - Check's message split between buffers in the following way:
 # <some_text>warning :
 # <some_text>check_text
-generateStringForDifferentBufferSize $((${#testStrSource} - ${#checkNameSource} - ${#commonCheckPart} / 2 )) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+generateStringForDifferentBufferSize $((${#testStrSource} - ${newLineSymSize} - \
+                                     ${#checkNameSource} - ${#commonCheckPart} / 2 )) \
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Test 11 - Check's message completely in buffer
 generateStringForDifferentBufferSize $((${#testStrSource} + 10)) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Test 12 - Two check's message completely in buffer
 generateStringForDifferentBufferSize $((${#testStrSource} * 2)) \
                                      "$testStrSource$testStrSource" \
-                                     "$testStrConverted$testStrConverted" $getTests
+                                     "$testStrConverted$testStrConverted" $getTests 2
 
 # Test 13 - Two check's message completely in buffer, some text between messages exist
-generateStringForDifferentBufferSize $((${#testStrSource} * 2)) \
+generateStringForDifferentBufferSize $(((${#testStrSource}) * 2)) \
                                      "${testStrSource}"HelloWorld"${testStrSource}" \
-                                     "${testStrConverted}"HelloWorld"${testStrConverted}" $getTests
+                                     "${testStrConverted}"HelloWorld"${testStrConverted}" $getTests 2
 
 # Test 14 - Test that all hyphen removed from check's name
 checkNameSource="[----wsome-check]"
 checkNameConverted="[wsome-check]"
-testStrSource="${pathToFile}${rowColumnSource}${commonCheckPart}${checkNameSource}"
-testStrConverted="${pathToFile}${rowColumnConverted}${commonCheckPart}${checkNameConverted}"
+testStrSource="${pathToFile}${rowColumnSource}${commonCheckPart}${checkNameSource}\n"
+testStrConverted="${pathToFile}${rowColumnConverted}${commonCheckPart}${checkNameConverted}\n"
 generateStringForDifferentBufferSize $((${#testStrSource} + 10)) \
-                                     "$testStrSource" "$testStrConverted" $getTests
+                                     "$testStrSource" "$testStrConverted" $getTests 1
 
 # Tests in which the text is very similar to the message that needs to be changed
 
 # Test 15
 rowColumnPart="(aa,bb):" # symbol instead of numbers
 checkNamePart="[wsome-check---]"
-testStr="${pathToFile}${rowColumnPart}${commonCheckPart}${checkNamePart}"
+testStr="${pathToFile}${rowColumnPart}${commonCheckPart}${checkNamePart}\n"
 generateStringForDifferentBufferSize $((${#testStr} + 10)) \
-                                     "${testStr}" "${testStr}" $getTests
+                                     "${testStr}" "${testStr}" $getTests 1
 
 # Test 16
 rowColumnPart="(42,    42):" # extra spaces added
 checkNamePart="[wsome----check]"
-testStr="${pathToFile}${rowColumnPart}${commonCheckPart}${checkNamePart}"
+testStr="${pathToFile}${rowColumnPart}${commonCheckPart}${checkNamePart}\n"
 generateStringForDifferentBufferSize $((${#testStr} + 10)) \
-                                     "${testStr}" "${testStr}" $getTests
+                                     "${testStr}" "${testStr}" $getTests 1
 
 # Test 17
 rowColumnPart="(42,42)" # colon does not exist
 checkNamePart="[wsome-check"
-testStr="${pathToFile}${rowColumnPart}${commonCheckPart}${checkNamePart}"
+testStr="${pathToFile}${rowColumnPart}${commonCheckPart}${checkNamePart}\n"
 generateStringForDifferentBufferSize $((${#testStr} + 10)) \
-                                     "${testStr}" "${testStr}" $getTests
+                                     "${testStr}" "${testStr}" $getTests 1
