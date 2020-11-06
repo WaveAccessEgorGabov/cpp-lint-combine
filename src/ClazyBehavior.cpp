9cd7b113acd234b7d9c00e42d6b8cf29b21a0f8b
EgorGabov <Egor.Gabov@waveaccess.ru>
2020-11-06 17:32:57 +0300

    Now we dont create reg. expr. for each class ClazyBehavior instance
    Also:
    1. Added required empty line at the end of files.
    2. In the class ClazyBehavior reg. expr. declaration was moved into ClazyBehavior::convertLinterOutput() and was marked as "static"

--- src/ClazyBehavior.cpp
+++ src/ClazyBehavior.cpp
@@ -1,7 +1,31 @@
 #include "ClazyBehavior.h"
+#include <regex>
 
 std::string LintCombine::ClazyBehavior::convertLinterOutput( std::string && linterOutputPart,
                                                              const ReadLinterOutputFrom readFrom ) {
+    static const std::regex s_conform{
+        #ifdef WIN32
+            // for UNC path starts with '\\'
+            R"(([a-zA-Z]:(?:[\\/][^\"\\*\\?\\/<>:|]*)*))" // 1: match windows-like file path
+        #else
+            "((/.*)*)" // 1: match unix-like file path
+        #endif
+            "(?:"                 // match row/column part in clazy style
+            "\\("
+            "(\\d+)"              // 2: match row number
+            ","
+            "(\\d+)"              // 3: match column number
+            "\\)"
+            "):"
+            "( "                  //
+            "(?:warning|error): " // 4: match check's level
+            ".* "                 //    match check's message
+            "\\["                 //
+            ")"                   //
+            "(-)*"                // match hyphens in the check's name begin
+            "(.*\\])",            // 5: match check's name
+            std::regex::optimize };
+
     std::string conversionResult;
     auto & currentWorkBuffer =
         readFrom == ReadLinterOutputFrom::Stdout ? m_stdoutBuffer : m_stderrBuffer;
@@ -14,7 +38,7 @@ std::string LintCombine::ClazyBehavior::convertLinterOutput( std::string && lint
     currentWorkBuffer += linterOutputPart.substr( 0, pos + 1 );
     while( pos != std::string::npos ) {
         conversionResult +=
-            std::regex_replace( currentWorkBuffer, m_regexForConversion, "$1:$4:$5:$6$9" );
+            std::regex_replace( currentWorkBuffer, s_conform, "$1:$2:$3:$4$6" );
 
         const auto oldPos = pos;
         pos = linterOutputPart.find( "\n", pos + 1 );
--- src/ClazyBehavior.h
+++ src/ClazyBehavior.h
@@ -2,8 +2,6 @@
 
 #include "LinterBehaviorItf.h"
 
-#include <regex>
-
 namespace LintCombine {
     class ClazyBehavior final : public LinterBehaviorItf {
     public:
@@ -13,27 +11,5 @@ namespace LintCombine {
     private:
         std::string m_stdoutBuffer;
         std::string m_stderrBuffer;
-
-        const std::regex m_regexForConversion{
-        #ifdef WIN32
-            "([a-zA-Z]:([\\\\/][^\\\"\\*\\?\\\\/<>:|]*)*)" // 1: match windows-like file path
-        #else
-            "((/.*)*)" // 1: match unix-like file path
-        #endif
-            "("      // match row/column part in clazy style
-            "\\("
-            "(\\d+)" // 4: match row number
-            ","
-            "(\\d+)" // 5: match column number
-            "\\)"
-            "):"
-            "( "                //
-            "(warning|error): " // 6: match check's level
-            ".* "               //    match check's message
-            "\\["               //
-            ")"                 //
-            ""
-            "(-)*"              // match hyphens in the check's name begin
-            "(.*\\])", std::regex::optimize };  // 9: match check's name
     };
 }
