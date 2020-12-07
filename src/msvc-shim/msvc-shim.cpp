61bf5773cab0ad3561ae764730bf6557b5186a37
EgorGabov <Egor.Gabov@waveaccess.ru>
2020-12-07 18:05:52 +0300

    msvc-shim was updated to reading command line options in UTF-16

--- src/msvc-shim/msvc-shim.cpp
+++ src/msvc-shim/msvc-shim.cpp
@@ -1,10 +1,11 @@
-#include <string>
+﻿#include <string>
 #include <filesystem>
+#include <process.h>
 
-int main( int argc, char ** argv ) {
-    std::string childProcessCmdLine =
-        "\"" + std::filesystem::path( argv[0] ).parent_path().string() +
-        "/clang-tidy/cpp-lint-combine-msvc.cmd\"";
-    for( int i = 1; i < argc; childProcessCmdLine += " " + std::string( argv[i++] ) );
-    return std::system( childProcessCmdLine.c_str() );
+int wmain( int argc, wchar_t ** argv ) {
+    std::wstring childProcessCmdLine =
+        L"\"" + std::filesystem::path( argv[0] ).parent_path().wstring() +
+        L"/clang-tidy/cpp-lint-combine-msvc.cmd\"";
+    for( int i = 1; i < argc; childProcessCmdLine += L" " + std::wstring( argv[i++] ) );
+    return _wsystem( childProcessCmdLine.c_str() );
 }
